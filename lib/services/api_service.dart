af49d281de1b66979182307d2788fa5eb75d0651
Amansarfraz <amansarfraz05@gmail.com>
2026-05-16 18:58:12 +0500

    first commit

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -42,7 +42,7 @@ class ApiService {
   // ================================
   // ANDROID EMULATOR
   // ================================
-  static const String baseUrl = "http://10.0.2.2:8000";
+  static const String baseUrl = "http://127.0.0.1:8000";
 
   // ================================
   // PHYSICAL DEVICE
